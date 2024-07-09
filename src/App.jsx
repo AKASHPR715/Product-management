36a6bc9ae4ad85a63df28bc08060a14aa889ca99
Akash <akashpr715@gmail.com>
2024-07-09 18:20:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import View from './components/View'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <View/>
+    </>
+  )
+}
+
+export default App
